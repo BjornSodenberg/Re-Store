00a5f5c6018f1ba2e93a23c1847b5a9a9625dc91
Yury Ognev <garonini1@gmail.com>
2020-08-19 12:11:33 +0700

    добавил таблицу заказов и стили к ней

--- src/components/shop-header/index.js
+++ src/components/shop-header/index.js
@@ -5,7 +5,7 @@ const ShopHeader = ({numItems, total}) => {
     return (
         <header className="shop-header row">
             <a className="logo text-dark" href="#c">ReStore</a>
-            <a className="shopping-cart">
+            <a className="shopping-cart" href="#c">
                 <i className="cart-icon fa fa-shopping-cart" />
                 {numItems} items (${total})
             </a>
