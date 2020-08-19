13345691bc4ea55ec809882ffeac058c53ea6f06
Yury Ognev <garonini1@gmail.com>
2020-08-19 11:33:51 +0700

    Добавил стиль для header и списка книг

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './app.css';
-import withBookstoreService from '../hoc';
+import ShopHeader from '../shop-header';
 import {Route, Switch} from 'react-router-dom';
 import {HomePage, CartPage} from '../pages';
 
 const App = ({bookstoreService}) => {
     
     return (
-        <Switch>
-            <Route path='/' component={HomePage} exact />
-            <Route path='/cart' component={CartPage} />
-        </Switch>
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={200}/>
+            <Switch>
+                <Route path='/' component={HomePage} exact />
+                <Route path='/cart' component={CartPage} />
+            </Switch>
+        </main>
     )
 };
 
