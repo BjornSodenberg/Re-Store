153eae4b38cb0c64284c7ddaa82ad3789b1ae87c
Yury Ognev <ognevyury.nsk@gmail.com>
2020-08-24 15:51:29 +0700

    сделал чтобы книжки стакались в корзине

--- src/components/book-list/index.js
+++ src/components/book-list/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
 import { withBookstoreService } from "../hoc";
-import { fetchBooks, onAddedToCart, bookAddedToCart } from "../../actions";
+import { fetchBooks, bookAddedToCart } from "../../actions";
 import { compose } from "../../utils";
 import Spinner from "../spinner";
 import ErrorIndicator from "../error-indicator";
